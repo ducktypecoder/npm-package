0814b0c7c071437387a1d55cbded4a3e0a76c729
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-05 18:00:29 -0500

    npm package will run the tests for a given step as included in this package.

--- projects/hello-world/step-1.js
+++
@@ -1,9 +0,0 @@
-var perform = require('../../lib/test-framework');
-
-var tests = [
-  ['finds the correct files', true, true],
-  ['finds the correct functions and objects', true, true],
-  ['functions work as expected', true, true]
-];
-
-module.exports = () => perform(tests);
---
+++ projects/hello-world/step-1.test.js
@@ -0,0 +1,13 @@
+it('works with jest', () => {
+  expect(true).toBe(true);
+})
+
+// var perform = require('../../lib/test-framework');
+
+// var tests = [
+//   ['finds the correct files', true, true],
+//   ['finds the correct functions and objects', true, true],
+//   ['functions work as expected', true, true]
+// ];
+//
+// module.exports = () => perform(tests);
