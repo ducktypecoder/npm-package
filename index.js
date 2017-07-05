65d90c8d02dee01594f72caf7813c186873ed3bb
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-05 15:17:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var getConfig = require('./lib/get-config');
+var updateUserProgress = require('./lib/update-user-progress');
+var { spawn } = require('child_process');
+var pwd = spawn('pwd');
+var projectDirectory;
+var config;
+
+// run a quick child process and grab the working project directory
+pwd.stdout.on('data', (data) => {
+  projectDirectory = `${data}`;
+  pwd.kill()
+});
+
+// after we kill the child process, proceed, knowing the workign project directory
+pwd.stdout.on('close', (code) => {
+  config = getConfig(projectDirectory);
+
+  updateUserProgress(config)
+    .then(() => {
+      console.log('finished!')
+      process.exit(0)
+    });
+})
