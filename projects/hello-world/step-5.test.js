28af43972f22a89e7c08217c8e50ddd6293d5b3b
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-07 11:36:00 -0500

    Changes 'hello-world' step 5

--- projects/hello-world/step-5.test.js
+++ projects/hello-world/step-5.test.js
@@ -3,8 +3,7 @@ var addNumbers = require('./src/add-numbers');
 
 it('returns NaN when appropriate', () => {
   const num1 = 3;
-  const num2 = {};
+  const num2 = '4';
 
-  const result = addNumbers(num1, num2);
-  expect(isNaN(result)).toEqual(true);
+  expect(addNumbers(num1, num2)).toEqual(7);
 });
