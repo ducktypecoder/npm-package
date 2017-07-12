03d56783a9e80ebb7548216e24ffa74be0630448
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-12 17:41:31 -0500

    package will get step test suite from server and write the file to the local directory instead of using hardcoded tests in a 'projects' directory.

--- projects/hello-world/step-4.test.js
+++
@@ -1,8 +0,0 @@
-var addNumbers = require('./src/add-numbers');
-
-// second, test that it works:
-it('adds numbers', () => {
-  const num1 = 3;
-  const num2 = 4;
-  expect(addNumbers(num1, num2)).toEqual(7);
-});
--- projects/hello-world/step-5.test.js
+++
@@ -1,9 +0,0 @@
-// Now let's test that add number returns NaN if necessary
-var addNumbers = require('./src/add-numbers');
-
-it('returns NaN when appropriate', () => {
-  const num1 = 3;
-  const num2 = '4';
-
-  expect(addNumbers(num1, num2)).toEqual(7);
-});
