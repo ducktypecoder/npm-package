b08e63f1b9c6722ff5666026de29b0850a4a4498
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-27 14:41:58 -0500

    Implements command 'init' to setup a blank ducktypecoder project. adds some packages for terminal output and using git.

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var chalk = require('chalk');
+var figlet = require('figlet');
 var nextStep = require('./lib/next-step');
+var initializeProject = require('./lib/initialize-project');
 
 program
   .version('0.0.1')
   .description('Create and follow projects on ducktypecoder')
   .parse(process.argv);
 
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('ducktypecoder', { horizontalLayout: 'full' })
+  )
+);
+
+
 switch (program.args[0]) {
   case 'next':
     nextStep();
     return;
-  default:
+  case 'init':
+    initializeProject(program.args);
+    return;
+  case undefined:
     console.log('no command provided...')
     return;
+  default:
+    console.log('command not recognized...')
+    return
 }
