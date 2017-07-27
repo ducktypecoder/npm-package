108affc59b3974bb06bba16b18fa76ec511a77f5
Mark Hurwitz <mhurwitzus@gmail.com>
2017-07-27 17:49:38 -0500

    Adds commands to 'add step', 'add answer', 'add conclusion'

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ var chalk = require('chalk');
 var figlet = require('figlet');
 var nextStep = require('./lib/next-step');
 var initializeProject = require('./lib/initialize-project');
+var addStep = require('./lib/add-step');
+var addAnswer = require('./lib/add-answer');
+var addConclusion = require('./lib/add-conclusion');
 
 program
   .version('0.0.1')
   .description('Create and follow projects on ducktypecoder')
   .parse(process.argv);
 
-
 console.log(
   chalk.yellow(
     figlet.textSync('ducktypecoder', { horizontalLayout: 'full' })
   )
 );
-
 
 switch (program.args[0]) {
   case 'next':
@@ -26,6 +27,14 @@ switch (program.args[0]) {
   case 'init':
     initializeProject(program.args);
     return;
+  case 'add':
+    var typeToAdd = program.args[1];
+
+    if (typeToAdd === 'step') addStep(program.args);
+    if (typeToAdd === 'answer') addAnswer(program.args);
+    if (typeToAdd === 'conclusion') addConclusion(program.args);
+
+    return
   case undefined:
     console.log('no command provided...')
     return;
